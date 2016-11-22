ab4c474e16822b735ceeccbfbbe625d0cd5cf187
LiliMeng <lilimeng1103@gmail.com>
2016-11-21 20:13:47 -0800

    Create Cross_product_normals.cpp

---
+++ Cross_product_normals.cpp
@@ -0,0 +1,48 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    const char* filename="depth_00000.png";
+    Mat depth=imread(filename, 0);
+
+
+    if(depth.type() != CV_32FC1)
+    {
+        depth.convertTo(depth, CV_32FC1);
+    }
+
+
+    Mat normals(depth.size(), CV_32FC3);
+
+    for(int x = 1; x < depth.cols - 1; ++x)
+    {
+        for(int y = 1; y < depth.rows - 1; ++y)
+        {
+            // 3d pixels, think (x,y, depth)
+             /* * * * *
+              * * t * *
+              * l c * *
+              * * * * */
+
+            Vec3f t(x,y-1,depth.at<float>(y-1, x));
+            Vec3f l(x-1,y,depth.at<float>(y, x-1));
+            Vec3f c(x,y,depth.at<float>(y, x));
+
+            Vec3f d = (l-c).cross(t-c);
+
+            Vec3f n = normalize(d);
+            normals.at<Vec3f>(y,x) = n;
+            //cout<<normals<<endl;
+        }
+    }
+
+    imshow("explicitly cross_product normals", normals);
+    waitKey();
+}
